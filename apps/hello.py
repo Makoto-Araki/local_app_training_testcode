5a624d1bcc344bf8af898e0584238cefbd613473
Makoto-Araki <mj-araki@ivy.ocn.ne.jp>
2026-02-28 14:42:55 +0900

    関数修正とテストケース修正

--- apps/hello.py
+++ apps/hello.py
@@ -11,8 +11,8 @@ def hello(name: str):
     # ログ出力
     logger.info('hello called')
 
-    # 引数がHelloまたはhelloの場合は例外発生
-    if name == 'Hello' or name == 'hello':
+    # 引数が大文字小文字を問わずHello場合は例外発生
+    if name.lower() == 'hello':
         logger.error('argument is invalid')
         raise ValueError('argument is invalid')
 
