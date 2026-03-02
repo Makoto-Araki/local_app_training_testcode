097cfe256946faccdce24988d02fac4e85780ba2
Makoto-Araki <mj-araki@ivy.ocn.ne.jp>
2026-03-03 07:45:08 +0900

    ヘッドレスモード導入

--- apps/google_controller.py
+++ apps/google_controller.py
@@ -5,7 +5,7 @@ from apps.google_service import GoogleService
 # RPA - レイヤー構成 - コントローラ層
 # --------------------------------------------------
 def run():
-    client = GoogleRpaClient()
+    client = GoogleRpaClient(headless=True)
     service = GoogleService(client)
 
     title = service.execute()
--- apps/google_rpa_client.py
+++ apps/google_rpa_client.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # --------------------------------------------------
 # RPA - レイヤー構成 - ゲートウェイ層
@@ -7,8 +8,19 @@ class GoogleRpaClient:
 
     GOOGLE_URL = "https://www.google.com"
 
-    def __init__(self, driver=None):
-        self.driver = driver or webdriver.Chrome()
+    def __init__(self, driver=None, headless: bool = True):
+        self.driver = driver or self._create_driver(headless)
+
+    def _create_driver(self, headless: bool):
+        options = Options()
+
+        if headless:
+            options.add_argument("--headless=new")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
+        return webdriver.Chrome(options=options)
 
     def open_top_page(self) -> None:
         self.driver.get(self.GOOGLE_URL)
